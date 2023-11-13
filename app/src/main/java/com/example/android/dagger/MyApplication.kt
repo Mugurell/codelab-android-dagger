2b251ce4c7b24ae5ae76164ba191f0af6a2cafad
Petru <petru-mugurel.lingurar@ing.com>
2023-11-13 23:51:17 +0200

    KI - Add the main component for the app
    This showcases the API of all future components.

--- app/src/main/java/com/example/android/dagger/MyApplication.kt
+++ app/src/main/java/com/example/android/dagger/MyApplication.kt
@@ -18,18 +18,17 @@ package com.example.android.dagger
 
 import android.app.Application
 import com.example.android.dagger.di.AppComponent
-import com.example.android.dagger.di.DaggerAppComponent
+import com.example.android.dagger.di.AppComponentProvider
+import com.example.android.dagger.di.create
 
-open class MyApplication : Application() {
+open class MyApplication : Application(), AppComponentProvider {
 
     // Instance of the AppComponent that will be used by all the Activities in the project
-    val appComponent: AppComponent by lazy {
-        initializeComponent()
-    }
+    override val appComponent: AppComponent by lazy { initializeComponent() }
 
     open fun initializeComponent(): AppComponent {
         // Creates an instance of AppComponent using its Factory constructor
         // We pass the applicationContext that will be used as Context in the graph
-        return DaggerAppComponent.factory().create(applicationContext)
+        return AppComponent.create(applicationContext)
     }
 }
--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -17,28 +17,26 @@
 package com.example.android.dagger.di
 
 import android.content.Context
-import com.example.android.dagger.login.LoginComponent
-import com.example.android.dagger.registration.RegistrationComponent
 import com.example.android.dagger.user.UserManager
-import dagger.BindsInstance
-import dagger.Component
+import me.tatarka.inject.annotations.Component
+import me.tatarka.inject.annotations.Provides
 
 // Scope annotation that the AppComponent uses
 // Classes annotated with @Singleton will have a unique instance in this Component
 @Singleton
-// Definition of a kotlin-inject component that adds info from the different modules to the graph
-@Component(modules = [StorageModule::class, AppSubcomponents::class])
-interface AppComponent {
+// Definition of a the kotlin-inject component that can be the parent of all others in the graph
+@Component
+abstract class AppComponent(
+    @get:Provides val context: Context
+) {
+    abstract val userManager: UserManager
 
-    // Factory to create instances of the AppComponent
-    @Component.Factory
-    interface Factory {
-        // With @BindsInstance, the Context passed in will be available in the graph
-        fun create(@BindsInstance context: Context): AppComponent
-    }
-
-    // Types that can be retrieved from the graph
-    fun registrationComponent(): RegistrationComponent.Factory
-    fun loginComponent(): LoginComponent.Factory
-    fun userManager(): UserManager
+    companion object
 }
+
+interface AppComponentProvider {
+    val appComponent: AppComponent
+}
+
+val Context.applicationComponent
+    get() = (applicationContext as AppComponentProvider).appComponent
