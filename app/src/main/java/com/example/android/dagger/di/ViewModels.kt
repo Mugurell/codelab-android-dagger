40cdce2b0ec694277783105b6da767063bbc6088
Petru <petru-mugurel.lingurar@ing.com>
2023-11-14 00:21:16 +0200

    KI - Add a kotlin-inject ViewModelFactory
    This will support providing custom dependencies to ViewModels similar to the
    Dagger implementation.

--- app/src/main/java/com/example/android/dagger/di/ViewModels.kt
+++ app/src/main/java/com/example/android/dagger/di/ViewModels.kt
@@ -1,40 +1,43 @@
-package com.example.android.dagger.di
-
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelStoreOwner
+import com.example.android.dagger.di.ActivityScope
+import com.example.android.dagger.di.AppComponent
 import com.example.android.dagger.settings.SettingsViewModel
-import dagger.Binds
-import dagger.MapKey
-import dagger.Module
-import dagger.multibindings.IntoMap
+import me.tatarka.inject.annotations.Component
 import me.tatarka.inject.annotations.Inject
-import javax.inject.Provider
-import kotlin.reflect.KClass
+import me.tatarka.inject.annotations.IntoMap
+import me.tatarka.inject.annotations.Provides
 
+@ActivityScope
 @Inject
 class ViewModelFactory(
-    private val viewModels: MutableMap<Class<out ViewModel>, Provider<ViewModel>>
+    private val viewModels: Map<Class<out ViewModel>, Lazy<ViewModel>>
 ) : ViewModelProvider.Factory {
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T =
-        viewModels[modelClass]?.get() as T
+        viewModels[modelClass]?.value as T
 }
 
-@Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
-@Retention(AnnotationRetention.RUNTIME)
-@MapKey
-internal annotation class ViewModelKey(val value: KClass<out ViewModel>)
+@ActivityScope
+@Component
+abstract class ViewModelComponent(@Component val parent: AppComponent) {
+    abstract val viewModelsMap: Map<Class<out ViewModel>, Lazy<ViewModel>>
+    abstract val viewModelFactory: ViewModelProvider.Factory
 
-@Module
-abstract class ViewModelModule {
+    @Provides
+    protected fun viewModelFactory(factory: ViewModelFactory): ViewModelProvider.Factory = factory
 
-    @Binds
-    internal abstract fun bindViewModelFactory(factory: ViewModelFactory): ViewModelProvider.Factory
-
-    @Binds
     @IntoMap
-    @ViewModelKey(SettingsViewModel::class)
-    internal abstract fun settingsViewModel(viewModel: SettingsViewModel): ViewModel
+    @Provides
+    protected fun settingsViewModel(vm: SettingsViewModel): Pair<Class<out ViewModel>, Lazy<ViewModel>> =
+        SettingsViewModel::class.java to lazyOf(vm)
 
-    //Add more ViewModels here
+    // Get any ViewModel existing in the kotlin-inject graph automatically constructed
+    // with all needed dependencies.
+    inline fun <reified VM : ViewModel> get(
+        owner: ViewModelStoreOwner
+    ) = lazyOf(ViewModelProvider(owner, viewModelFactory)[VM::class.java])
+
+    companion object
 }
